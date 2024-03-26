3d928bf3ea033d637070e93f31db02cd1cff040c
jessicahattig <jessicahattig@gmail.com>
2024-03-25 20:36:15 -0700

    Add components and WIP App.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Picture from './Picture';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      < TopStories />
+    </React.Fragment>
+  );
+}
+
+export default App;
