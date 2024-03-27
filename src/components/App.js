a32a4e18bb529665467f48d4b98ed26fa3d4bde3
jessicahattig <jessicahattig@gmail.com>
2024-03-26 20:41:54 -0700

    BUGFIX: App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import Picture from './Picture';
 
 
 function App() {
   return (
     <React.Fragment>
-      < TopStories />
+      < Picture />
     </React.Fragment>
   );
 }
